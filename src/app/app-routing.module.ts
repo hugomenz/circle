a1b510cef865c252ee3487ef559b5cbc66fa8de2
Hugo Martin <hugomartin.menz@gmail.com>
2022-05-16 19:45:43 +0200

    upload project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'profile', component:ProfileComponent},
+  {path:'joinnow', component:JoinnowComponent},
+  {path:'about', component:AboutComponent},
+  {path:'blog', component:BlogComponent},
+  {path:'contact', component:ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { CircleComponent } from './profile/circle/circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    BlogComponent,
+    AboutComponent,
+    ContactComponent,
+    JoinnowComponent,
+    CircleComponent
   ],
   imports: [
     BrowserModule,
